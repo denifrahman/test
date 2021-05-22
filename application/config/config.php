ad0d3439578f4a5712578185bb6e4bdb2dbbc98b
denifrahman <denifrahman@gmail.com>
2021-05-22 11:08:27 +0700

    test

--- application/config/config.php
+++ application/config/config.php
@@ -23,10 +23,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = ((isset($_SERVER['HTTP']) && $_SERVER['HTTPS'] == "on") ? "http" : "https");
-// $config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
-// $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']); 
- $config['base_url'] = "http://localhost/test-praktek";
+$config['base_url'] = ((isset($_SERVER['HTTP']) && $_SERVER['HTTP'] == "on") ? "http" : "http");
+$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']); 
+//  $config['base_url'] = "http://localhost/test-praktek";
 /*
 |--------------------------------------------------------------------------
 | Index File
